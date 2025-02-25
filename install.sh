1c80a0dfa3bf8d0b71e6521db843758e07317569
Konstantin Volodin <volodin.konstantin@gmail.com>
2025-02-25 10:16:19 +0000

    +++

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt-get dist-upgrade -y
 
 sudo apt-get install -y postgresql postgresql-contrib
 sudo systemctl start postgresql.service
-sudo systemctl status postgresql.service
+# sudo systemctl status postgresql.service
 
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
