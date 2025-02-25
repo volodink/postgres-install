4b4383231fe345fa6f22b6b45a9b89632b6d2981
Konstantin Volodin <volodin.konstantin@gmail.com>
2025-02-25 11:23:50 +0000

    +++

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ PG_HBA_CONF="/etc/postgresql/16/main/pg_hba.conf"
 
 sudo sed -i 's/^#listen_addresses =.*/listen_addresses = '\''*'\''/' "$POSTGRESQL_CONF"
 
-sudo echo "host    all             all             0.0.0.0/0               md5" >> "$PG_HBA_CONF"
+sudo echo "local    all             all                            trust" > "$PG_HBA_CONF"
+sudo echo "host    all             all             all               trust" >> "$PG_HBA_CONF"
 
 sudo systemctl restart postgresql
 sudo service postgresql restart
